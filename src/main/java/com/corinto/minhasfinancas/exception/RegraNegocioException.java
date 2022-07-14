5872c2b14fc9f7828ae9887c48820ab423b2bc4e
FHOCFHOC <backintheusa@gmail.com>
2022-07-14 12:54:01 -0300

    Commit inicial

---
+++ src/main/java/com/corinto/minhasfinancas/exception/RegraNegocioException.java
@@ -0,0 +1,9 @@
+package com.corinto.minhasfinancas.exception;
+
+public class RegraNegocioException extends RuntimeException {
+
+	public RegraNegocioException(String msg) {
+		super(msg);
+	}
+
+}
---
+++ src/main/java/com/corinto/minhasfinancas/model/enums/StatusLancamento.java
@@ -0,0 +1,8 @@
+package com.corinto.minhasfinancas.model.enums;
+
+public enum StatusLancamento {
+
+	PENDENTE,
+	CANCELADO,
+	EFETIVADO
+}
